986036f19cec90cc625ff07c5719918b50678fdd
Evgeniy <lohlohko@gmail.com>
2025-05-17 18:35:17 +0000

    Fix CORS to allow Vercel

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routes import router  # бо файл routes.py лежить у backend/
+from backend.routes import router
 
 app = FastAPI(
     title="Event Registration API",
@@ -8,19 +8,19 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# Налаштування CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "https://event-registration-liart.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Підключення роутера
 app.include_router(router)
 
-# Запуск
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("backend.main:app", host="127.0.0.1", port=8000, reload=True)
